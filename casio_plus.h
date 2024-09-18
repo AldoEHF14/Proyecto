9aae8be526429c043c09d18296b79f36af902b4d
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-09-17 18:04:38 -0600

    add lex & yacc files

---
+++ casio_plus.h
@@ -0,0 +1,30 @@
+typedef enum { typeCon, typeId, typeOpr} nodeEnum;
+
+/* constants */
+typedef struct {
+   nodeEnum type;
+   int value;                  /* value of constant */
+} conNodeType;
+
+/* identifiers */
+typedef struct {
+   nodeEnum type;
+   char name[50];
+} idNodeType;
+
+/* operators */
+typedef struct {
+   nodeEnum type;
+   int oper;                   /* operator */
+   int nops;                   /* number of operands */
+   union nodeTypeTag *op[1];  /* operands (expandable) */
+} oprNodeType;
+
+typedef union nodeTypeTag {
+   nodeEnum type;              /* type of node */
+
+   conNodeType con;        /* constants */
+   idNodeType id;          /* identifiers */
+   oprNodeType opr;        /* operators */
+} nodeType;
+
